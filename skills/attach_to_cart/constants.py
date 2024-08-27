0a0d3ee46d85880e7602b6ec4515baf2fbd89a94
Santiago Barragan <santiago@unlimited-robotics.com>
2024-08-27 11:17:45 -0500

    add missing constant

--- skills/attach_to_cart/constants.py
+++ skills/attach_to_cart/constants.py
@@ -7,3 +7,13 @@ FAMILY = '36h11'
 # TARGET_TAGS = ['1']
 TARGET_DISTANCE = 0.7
 WAIT_TIME_FOR_DETECTION = 10
+
+MODEL_PARAMS = {
+    'families' : 'tag36h11',
+    'nthreads' : 4,
+    'quad_decimate' : 2.0,
+    'quad_sigma': 0.0,
+    'decode_sharpening' : 0.25,
+    'refine_edges' : 1,
+    'tag_size' : 0.0
+}
